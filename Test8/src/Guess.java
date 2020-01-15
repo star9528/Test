bc04497cd27f75dce7d06b01967afbc023641cc4
飞 <1752840757@qq.com>
2020-01-15 17:13:20 +0800

    猜字游戏

---
+++ Test8/src/Guess.java
@@ -0,0 +1,28 @@
+//完成猜数字游戏
+import java.util.Random;
+import java.util.Scanner;
+public class Guess {
+    public static void toGuess() {
+        Random random = new Random();
+        int a = random.nextInt(101);
+
+
+        for (int i = 1; i < 100; i++) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("请输入你要猜的数字：");
+            int toGuess = scanner.nextInt();
+            if (a > toGuess) {
+                System.out.println("输入小了!");
+            } else {
+                if (a < toGuess) {
+                    System.out.println("输入大了！");
+                } else {
+                    System.out.println("恭喜你！猜对了！");
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+            toGuess();
+    }
+}
