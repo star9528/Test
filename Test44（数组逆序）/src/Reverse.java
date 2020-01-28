48eb7ee6be0d24eee5517ed829858d1abaa94cfa
飞 <1752840757@qq.com>
2020-01-28 16:56:10 +0800

    数组逆序

---
+++ Test44（数组逆序）/src/Reverse.java
@@ -0,0 +1,27 @@
+//数组逆序
+import java.util.Arrays;
+
+public class Reverse {
+    public static void toReverse(int[] arr){
+//        for(int i = 0;i < (arr.length / 2);i++){
+//            int temp = arr[arr.length - 1 - i];
+//            arr[arr.length - 1 - i] = arr[i];
+//            arr[i] = temp;
+//        }
+        int left = 0;
+        int right = arr.length - 1;
+        while(left < right){
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,8,9};
+        toReverse(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
