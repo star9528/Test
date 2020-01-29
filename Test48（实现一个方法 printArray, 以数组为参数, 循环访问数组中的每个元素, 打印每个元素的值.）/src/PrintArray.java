8fcd53493f30a67a8a74eb7ab14c06bafb99030a
飞 <1752840757@qq.com>
2020-01-29 17:34:30 +0800

    打印数组每一个元素

---
+++ Test48（实现一个方法 printArray, 以数组为参数, 循环访问数组中的每个元素, 打印每个元素的值.）/src/PrintArray.java
@@ -0,0 +1,14 @@
+//编写代码: 实现一个方法 printArray, 以数组为参数,
+// 循环访问数组中的每个元素, 打印每个元素的值.
+public class PrintArray {
+    public static void printArray(int[] arr){
+        for(int i = 0;i < arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6};
+        printArray(arr);
+    }
+}
